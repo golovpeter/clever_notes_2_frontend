1111052c75c85d8e8e7d1e65cebebb6fedf49f38
Голов Петр <petya-golov@mail.ru>
2022-08-23 13:04:17 +0300

    refactoring

--- src/components/InfCard/InfCard.js
+++ src/components/InfCard/InfCard.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import './InfCard.css'
 import {Card, Col, Row} from "antd";
+import {isMobile} from "react-device-detect";
 
 class InfCard extends React.Component {
     render() {
-        return (
-            <>
-                <Row type="flex" justify="center" align="middle" style={{paddingTop: "70px", paddingLeft:"250px"}}>
-                    <Col span={12}>
-                        <Card title={<p align="center" style={{fontSize: "25px"}}>Easy-way to create notes</p> }  style={{width: "70%"}}>
-                            <p>Simplified design and easy to use make this note service an indispensable helper in everyday life.</p>
-                        </Card>
-                    </Col>
-                    <Col span={12}>
-                        <Card title={<p align="center" style={{fontSize: "25px"}}>Have notes everywhere</p>} style={{width: "70%"}} >
-                            <p align="center">Cloud - system of notes allows you to use notes from different devices.</p>
-                        </Card>
-                    </Col>
-                </Row>
-            </>
-        );
+        if (!isMobile) {
+            return (
+                <>
+                    <Row type="flex" justify="center" align="middle" style={{paddingTop: "70px", paddingLeft: "250px"}}>
+                        <Col span={12}>
+                            <Card title={<p align="center" style={{fontSize: "25px"}}>Easy-way to create notes</p>}
+                                  style={{width: "70%"}}>
+                                <p>Simplified design and easy to use make this note service an indispensable helper in
+                                    everyday life.</p>
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                            <Card title={<p align="center" style={{fontSize: "25px"}}>Have notes everywhere</p>}
+                                  style={{width: "70%"}}>
+                                <p align="center">Cloud - system of notes allows you to use notes from different
+                                    devices.</p>
+                            </Card>
+                        </Col>
+                    </Row>
+                </>
+            );
+        }
     }
 }
 
