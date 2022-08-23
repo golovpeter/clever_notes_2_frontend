579347bb4f1a5ee68eecd597116209ee1c88550f
Голов Петр <petya-golov@mail.ru>
2022-08-23 16:51:37 +0300

    refactoring

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import './Footer.css'
 
-class PageFooter extends React.Component {
-    render() {
-        return(
+const PageFooter = () =>
             <div className="footer">
                 <div className="container">
                     <span className="footerText">Golov Peter. All rights reserved.</span>
                 </div>
             </div>
-        );
-    }
-}
 
 export default PageFooter
